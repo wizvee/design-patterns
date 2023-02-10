ea3fb10a2ce95b1f1d258f9b904a64c1d828fd22
wizvee <jeonghye.dev@gmail.com>
2023-02-10 23:44:24 +0900

    feat: composite

---
+++ src/composite.ts
@@ -0,0 +1,72 @@
+abstract class Component {
+  protected parent!: Component | null;
+
+  public setParent(parent: Component | null) {
+    this.parent = parent;
+  }
+
+  public getParent(): Component | null {
+    return this.parent;
+  }
+
+  public add(component: Component): void {}
+  public remove(component: Component): void {}
+
+  public isComposite(): boolean {
+    return false;
+  }
+
+  public abstract operation(): string;
+}
+
+class Leaf extends Component {
+  public operation(): string {
+    return "Leaf";
+  }
+}
+
+class Composite extends Component {
+  protected children: Component[] = [];
+
+  public add(component: Component): void {
+    this.children.push(component);
+    component.setParent(this);
+  }
+
+  public remove(component: Component): void {
+    const index = this.children.indexOf(component);
+    this.children.splice(index, 1);
+
+    component.setParent(null);
+  }
+
+  public isComposite(): boolean {
+    return true;
+  }
+
+  public operation(): string {
+    const results = this.children.map((child) => child.operation()).join(" + ");
+    return `Branch(${results})`;
+  }
+}
+
+function clientCode(component: Component) {
+  console.log(`Result: ${component.operation()}`);
+}
+
+const simple = new Leaf();
+console.log("Client: I've got a simple component:");
+clientCode(simple);
+console.log("");
+
+const tree = new Composite();
+const branch1 = new Composite();
+branch1.add(new Leaf());
+branch1.add(new Leaf());
+const branch2 = new Composite();
+branch2.add(new Leaf());
+tree.add(branch1);
+tree.add(branch2);
+console.log("Client: Now I've got a composite tree:");
+clientCode(tree);
+console.log("");
