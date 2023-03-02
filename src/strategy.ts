84c5a9c26721f1432271fdc04b6841e7f4465574
wizvee <jeonghye.dev@gmail.com>
2023-03-02 23:42:16 +0900

    feat: strategy

---
+++ src/strategy.ts
@@ -0,0 +1,45 @@
+class StrategyContext {
+  private strategy: Strategy;
+
+  constructor(strategy: Strategy) {
+    this.strategy = strategy;
+  }
+
+  public setStrategy(strategy: Strategy) {
+    this.strategy = strategy;
+  }
+
+  public doSomeBusinessLogic(): void {
+    console.log(
+      "Context: Sorting data using the strategy (not sure how it'll do it)"
+    );
+    const result = this.strategy.doAlgorithm(["a", "b", "c", "d", "e"]);
+    console.log(result.join(","));
+  }
+}
+
+interface Strategy {
+  doAlgorithm(data: string[]): string[];
+}
+
+class ConcreteStrategyA implements Strategy {
+  public doAlgorithm(data: string[]): string[] {
+    return data.sort();
+  }
+}
+
+class ConcreteStrategyB implements Strategy {
+  public doAlgorithm(data: string[]): string[] {
+    return data.reverse();
+  }
+}
+
+const strategyContext = new StrategyContext(new ConcreteStrategyA());
+console.log("Client: Strategy is set to normal sorting.");
+strategyContext.doSomeBusinessLogic();
+
+console.log("");
+
+console.log("Client: Strategy is set to reverse sorting.");
+strategyContext.setStrategy(new ConcreteStrategyB());
+strategyContext.doSomeBusinessLogic();
